e1e75e0db3294a8e167645640b947253e087bba7
kamyab <mohamad.khsvi@gmail.com>
2025-06-16 19:12:08 +0330

    Code clean up and camera toggle added

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,22 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
+        google()
+        maven {
+            url = uri("https://maven.myket.ir")
         }
+        maven { url = uri("https://jitpack.io") }
         mavenCentral()
         gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://maven.myket.ir")
+        }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
